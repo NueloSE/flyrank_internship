10c11b193172acd8421f051c1e6fe9544be07f48
nuelo <akalo.emmanuel18@gmail.com>
2026-07-24 12:43:23 +0100

    stage 2: read endpoints with 404

--- w2-crud/app.js
+++ w2-crud/app.js
@@ -2,12 +2,35 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+const tasks = [
+    {id: 1, title: 'pray', done: true},
+    {id: 2, title: 'eat', done: false},
+    {id: 3, title: 'bath', done: false}
+]
+
 app.get('/health', (req, res) => {
     res.json({ "status": "ok" })
 })
 
 app.get('/', (req, res) => {
     res.json({"name": "Task API", "version": "1.0", "endpoints": ["/tasks"] })
+})
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks)
+})
+
+app.get('/tasks/:id', (req, res) => {
+    
+        const selectedTask = tasks.filter((task) => task.id == req.params.id)
+
+        if (selectedTask.length === 0) {
+            res.status(404).json({ "error": "Task 99 not found"})
+        } else {
+            res.status(200).json(selectedTask)
+        }
+    
+    
 })
 
 
